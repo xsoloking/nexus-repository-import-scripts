f391351652d2527ada4792b5291dd8a9d77f6bfb
xsoloking <soloking.x@gmail.com>
2024-01-30 23:43:51 +0800

    Create rubyimport.sh

---
+++ rubyimport.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# copy and run this script to the root of the repository directory containing files
+# this script attempts to exclude uploading itself explicitly so the script name is important
+# Get command line params
+while getopts ":r:u:p:" opt; do
+	case $opt in
+		r) REPO_URL="$OPTARG"
+		;;
+		u) USERNAME="$OPTARG"
+		;;
+		p) PASSWORD="$OPTARG"
+		;;
+	esac
+done
+
+AUTH=$(echo -n "${USERNAME}:${PASSWORD}" | base64 -w 0)
+
+cat > ~/.gem/nexus << EOF
+---
+:url: $REPO_URL
+:authorization: Basic $AUTH
+EOF
+
+find . -type f -not -path '*/\.*' -name '*.gem' -exec gem nexus {} \;
